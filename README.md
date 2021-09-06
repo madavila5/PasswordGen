d595186fa1847b1b15db51940d3d923e03352d81
Miguel Davila <migueldavila@Miguels-Mac-mini.local>
2021-09-05 23:40:35 -0700

    Update to README,Style and script structure of functiones needed

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# PasswordGen
+# PasswordGenerator
 
 Password Generator Project
 
 - This Password Generator Project was done based on a starter code which was included in the first commit. 
 - This Generator allows the individual to generate a random password based on the prompts.
-
-
+- When asked to Generete Password, we are prompted to type a length of desired password (8-128 Characters)
+- We are also prompted to pick what type of characters should be included (Uppercase, Lowercase, Special Characters, Numbers)
+- Once prompt choices are made, generator will produce a secure password in box.
 
 # Skills Used
 
@@ -16,7 +17,8 @@ Password Generator Project
 
 # Images
 
-<img src="" alt="">
+<img src="images/Password1" alt="Password Generator Initial Screen">
 <img src="" alt="">
 
 # Link
+https://madavila5.github.io/PasswordGen/ 
--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-// Assignment Code
+// Assignment Code - original
 var generateBtn = document.querySelector("#generate");
+
+//var
+var upper = ["A","B","C","D","E","F","G",'H','I','J','K',
+  'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lower = ["a", "b","c","d","e","f","g","h","i","j",'k','l',
+  'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var special = ["!","@","#","$","%","&","*","?",">","<","+"];
+var number = ["0","1","2","3","4","5","6","7","8","9"]
+
+//const
+const upper = "ABCDEFGHUJKLMNOPQRSTUVWXYZ"
+const lower = "abcdefghijklmnopqrstuvwxyz"
+const special = "!@#$%&*?><+"
+const number = "0123456789"
+
+//function prompt length of characters
+function characterLength(){
+
+};
+
+//function prompt include uppercase
+function upCase(){
+
+}
+
+//function prompt include lowercase
+function loCase(){
+
+}
+
+//function promp include special
+function spCase(){
+
+}
+
+//function prompt include number
+function nuCase(){
+  
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -12,5 +51,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
